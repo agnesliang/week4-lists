ffdf6cef320173247ab55df13c605f64db6ec2e4
agnesliang <agnesliang@gmail.com>
2021-03-02 18:30:59 -0800

    added code

--- week4listchallenge.py
+++ week4listchallenge.py
@@ -1,36 +1,24 @@
 # Black-Owned Restaurant Finder: Lists exercises
 
-####################################################################################################
-# Think about different cuisines (as many as you want) and store them in a list. Using what you
-# learned in the previous exercises, write code that asks the user what cuisine they are interested
-# in. If the cuisine they entered is not in the list, print the string "Please choose one of these
-# cuisines: [LIST OF CUISINES HERE]".
+#Part 1-
 
-# YOUR CODE HERE
+cuisines = {'Japanese', 'Korean', 'Italian', 'Mexican', 'Greek', 'Argentenian', 'French'}
+cuisine = input ('What cuisine are you interested in?')
 
-####################################################################################################
-# Find the name of at least three black-owned restaurants nearby and store their names in a list.
-# Then, print out the name of each restaurant, one line at a time.
+if cuisine in cuisines:
+    print ('Restaurant found!')
+else: 
+    print ('Please choose one of these cuisines from this list {cuisines}')
 
-# TIP: Use Google Maps to find nearby black-owned restaurants
-# https://www.google.com/maps/search/black+owned+restaurants
+#Part 2
 
-# YOUR CODE HERE
+black_owned_restaurants = ['Hard Knox Cafe', 'Bella Trattoria', 'Taqueria Los Mayas']
+for i in black_owned_restaurants:
+    print(black_owned_restaurants.index(i) +1, end = '')
+    print('', i)
 
-####################################################################################################
-# Take your list of restaurants from the previous challenge and print them out in a numbered list.
-# Example:
-# 1. <RESTAURANT 1>
-# 2. <RESTAURANT 2>
-# 3. <RESTAURANT 3>
+distance_from_restaurant = [2, 4, 6]
 
-# YOUR CODE HERE
-
-####################################################################################################
-# Find out how far away each restaurant is (in miles) and store those distances in another list.
-# Then, print out the name of each restaurant AND how far away it is in a numbered list. Example:
-# 1. <RESTAURANT 1> is 2.4 miles away
-# 2. <RESTAURANT 2> is 6.8 miles away
-# 3. <RESTAURANT 3> is 10.2 miles away
-
-# YOUR CODE HERE
+for i in range(len(distance_from_restaurant)):
+    print (i +1, black_owned_restaurants[i]+ 'is' + str(distance_from_restaurant[i])+'Miles Away')
+    print (f'{i+1} {black_owned_restaurants[i]} is {str(distance_from_restaurant [i])} Miles Away')
